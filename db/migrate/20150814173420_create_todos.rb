7c28c18abafb012babce8e6da89966058db605a1
Sujay Maheshwari <taurusismysign@gmail.com>
2015-08-19 00:15:18 -0700

    Initial Commit

---
+++ db/migrate/20150814173420_create_todos.rb
@@ -0,0 +1,12 @@
+class CreateTodos < ActiveRecord::Migration
+  def change
+    create_table :todos do |t|
+      t.string :description
+      t.boolean :is_completed
+      t.datetime :target_date
+      t.string :category
+
+      t.timestamps null: false
+    end
+  end
+end
